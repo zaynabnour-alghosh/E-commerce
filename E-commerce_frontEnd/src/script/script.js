a28fa6c783d98c3988a893cfc60af91b499713de
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-07-31 01:11:41 +0300

    implementing login api

--- E-commerce_frontEnd/src/script/script.js
+++ E-commerce_frontEnd/src/script/script.js
@@ -23,3 +23,39 @@ pages.postAPI = async(api_url, api_data) => {
 pages.loadFor = (page) => {
     eval("pages.page_" + page + "();")
 }
+
+pages.page_index = () => {
+    console.log('sign in')
+    const emailInput = document.getElementById("email")
+    const passwordInput = document.getElementById("password")
+    const btnSignIn=document.getElementById('btnSignin')
+    const errorMail=document.querySelector(".error-mail")
+    const errorPass=document.querySelector(".error-pass")
+    btnSignIn.addEventListener("click",async()=>{
+        if (emailInput.value==="" ||passwordInput.value===""){
+            console.log("empty inputs")
+            errorMail.style.display="block";
+            errorPass.style.display="block";
+        }
+        else if(emailInput!=="" ||passwordInput!==""){
+            errorMail.style.display="none";
+            errorPass.style.display="none";
+            // console.log(emailInput.value,passwordInput.value);
+        
+            console.log('clicked')
+                const email = emailInput.value;
+                const pass = passwordInput.value;
+                const data = new FormData()
+                data.append("email", email)
+                data.append("password", pass)
+                const response = await axios.post(pages.base_url + "login", data);
+                // console.log(response.data);
+                if(response.data){
+                    userId=response.data.user.id;
+                    token=response.data.authorization.token
+                    localStorage.setItem("userId", JSON.stringify(userId))
+                    localStorage.setItem("token",JSON.stringify(token))
+                    window.location.href = "dashboard.html"           }    
+        }
+    });
+}
